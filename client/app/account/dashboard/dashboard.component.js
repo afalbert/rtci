04da8f30c452e3e35556a4713460ead2611ca604
mziyambi <mziyam@mtc.ca.gov>
2018-03-04 09:46:41 -0800

    [Charts] Updates

--- client/app/account/dashboard/dashboard.component.js
+++ client/app/account/dashboard/dashboard.component.js
@@ -22,7 +22,7 @@ export class DashboardComponent {
                 console.log(this.assetsSummary);
 
                 var test = _.map(this.assetsSummary, 'ModeCode');
-                loadHighCharts(test);
+                // loadHighCharts(test);
                 console.log(test);
             })
             .catch(err => {
@@ -32,83 +32,199 @@ export class DashboardComponent {
 
         // High Charts
 
-        function loadHighCharts(data) {
-            Highcharts.chart('highchartsContainer', {
-                chart: {
-                    type: 'bar'
+        Highcharts.chart('container', {
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: 'Percent of Revenue Vehicles Above Useful Life'
+            },
+            colors: ['#db2929', '#00738C', '#307189', '#910000', '#1aadce',
+              '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'
+            ],
+            xAxis: {
+              categories: ['Light Rail/Trolley/Cable Car', 'Ferry', 'Bus', 'Heavy Rail', 'Cutaway/Van/Paratransit']
+            },
+            yAxis: {
+              min: 0,
+              title: {
+                text: 'Total Percentage (%)'
+              }
+            },
+            tooltip: {
+              pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+              shared: true
+            },
+            plotOptions: {
+              column: {
+                stacking: 'percent'
+              }
+            },
+            series: [{
+              name: 'Above Useful Life',
+              data: [2, 2, 3, 2, 1]
+            }, {
+              name: 'Within Useful Life',
+              data: [5, 3, 4, 7, 2]
+            }]
+          });
+      
+          // Pie Chart
+      
+      
+          // Make monochrome colors
+          var pieColors = (function () {
+            var colors = [],
+              base = '#00738C',
+              i;
+      
+            for (i = 0; i < 10; i += 1) {
+              // Start out with a darkened base color (negative brighten), and end
+              // up with a much brighter color
+              colors.push(Highcharts.Color(base).brighten((i - 3) / 12).get());
+            }
+            return colors;
+          }());
+      
+          // Build the chart
+          Highcharts.chart('pieChartContainer', {
+            chart: {
+              plotBackgroundColor: null,
+              plotBorderWidth: null,
+              plotShadow: false,
+              type: 'pie'
+            },
+            title: {
+              text: 'Breakdown of Revenue Vehicles by Mode'
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+              pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                colors: pieColors,
+                dataLabels: {
+                  enabled: true,
+                  format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                  // distance: -50,
+                  filter: {
+                    property: 'total',
+                    operator: '>',
+                    value: 4
+                  }
+                }
+                // showInLegend: true
+              }
+            },
+            series: [{
+              name: 'Brands',
+              data: [{
+                  name: 'Bus',
+                  y: 5963
                 },
-                title: {
-                    text: 'Stacked bar chart'
+                {
+                  name: 'Cutaway/Van/Paratransit',
+                  y: 1374
                 },
-                colors: ['#6D9D46', '#8D569B', '#307189', '#910000', '#1aadce',
-                    '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'
-                ],
-                xAxis: {
-                    categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+                {
+                  name: 'Ferry',
+                  y: 101
                 },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Total fruit consumption'
-                    }
+                {
+                  name: 'Heavy Rail',
+                  y: 1110
                 },
-                legend: {
-                    reversed: true
-                },
-                plotOptions: {
-                    series: {
-                        stacking: 'normal'
-                    }
-                },
-                series: [{
-                    name: 'John',
-                    data: [5, 3, 4, 7, 2]
-                }, {
-                    name: 'Jane',
-                    data: [2, 2, 3, 2, 1]
-                }, {
-                    name: 'Joe',
-                    data: [3, 4, 4, 2, 5]
-                }]
-            });
-        }
+                {
+                  name: 'Light Rail/Trolley/Cable Car',
+                  y: 604
+                }
+      
+              ]
+            }]
+          });
+      
+          //Electric Vehicles
+      
+    //   Highcharts.chart('electricVehiclesContainer', {
+    //       chart: {
+    //           type: 'bar'
+    //       },
+    //       title: {
+    //           text: 'Fleet Breakdown by Fuel Type'
+    //       },
+    //       colors: ['#7CFC00', '#629632', '#00738C', '#910000', '#1aadce',
+    //       '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'
+    //   ],
+    //       xAxis: {
+    //           categories: ['AC Transit', 'CCCTA', 'Delta Breeze', 'Dixon Readi-Ride', 'ECCTA','FAST','Golden Gate Transit','LAVTA','Marin Transit','NVTA','Petaluma Transit','SamTrans','Santa Rosa Transit','SCT','SF Muni','Solano County Transit','UCT','Vacaville','VTA','WestCAT']
+    //       },
+    //       yAxis: {
+    //           min: 0,
+    //           max:100,
+    //           title: {
+    //               text: 'Fuel Type Percent of Total'
+    //           }
+    //       },
+    //       legend: {
+    //           reversed: true
+    //       },
+    //       plotOptions: {
+    //           series: {
+    //               stacking: 'normal',
+    //               pointWidth:25
+    //           }
+    //       },
+    //       series: [ {
+    //           name: 'Zero Emissions',
+    //           data: [5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5]
+    //       },{
+    //           name: 'Hybrid',
+    //           data: [5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5]
+    //       },{
+    //           name: 'Diesel/Combustion Fuel',
+    //           data: [90, 90, 90, 90, 90,90, 90, 90, 90, 90,90, 90, 90, 90, 90,90, 90, 90, 90, 90]
+    //       }]
+    //   });
+      
         // End High Charts
 
-        var vizList = ["http://public.tableau.com/views/RegionalSampleWorkbook/Flights",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/Obesity",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/College",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/Stocks",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/Storms"
-        ];
+        // var vizList = ["http://public.tableau.com/views/RegionalSampleWorkbook/Flights",
+        //     "http://public.tableau.com/views/RegionalSampleWorkbook/Obesity",
+        //     "http://public.tableau.com/views/RegionalSampleWorkbook/College",
+        //     "http://public.tableau.com/views/RegionalSampleWorkbook/Stocks",
+        //     "http://public.tableau.com/views/RegionalSampleWorkbook/Storms"
+        // ];
 
-        var viz,
-            vizLen = vizList.length,
-            vizCount = 0;
+        // var viz,
+        //     vizLen = vizList.length,
+        //     vizCount = 0;
 
-        function createViz(vizPlusMinus) {
-            var vizDiv = document.getElementById("vizContainer"),
-                options = {
-                    hideTabs: true
-                };
+        // function createViz(vizPlusMinus) {
+        //     var vizDiv = document.getElementById("vizContainer"),
+        //         options = {
+        //             hideTabs: true
+        //         };
 
-            vizCount = vizCount + vizPlusMinus;
+        //     vizCount = vizCount + vizPlusMinus;
 
-            if (vizCount >= vizLen) {
-                // Keep the vizCount in the bounds of the array index.
-                vizCount = 0;
-            } else if (vizCount < 0) {
-                vizCount = vizLen - 1;
-            }
+        //     if (vizCount >= vizLen) {
+        //         // Keep the vizCount in the bounds of the array index.
+        //         vizCount = 0;
+        //     } else if (vizCount < 0) {
+        //         vizCount = vizLen - 1;
+        //     }
 
-            if (viz) { // If a viz object exists, delete it.
-                viz.dispose();
-            }
+        //     if (viz) { // If a viz object exists, delete it.
+        //         viz.dispose();
+        //     }
 
-            var vizURL = vizList[vizCount];
-            viz = new tableau.Viz(vizDiv, vizURL, options);
-        }
+        //     var vizURL = vizList[vizCount];
+        //     viz = new tableau.Viz(vizDiv, vizURL, options);
+        // }
 
-        createViz(3);
+        // createViz(3);
 
         // this.$http.get('/api/users/getAssets/Total')
         //     .then(result => {
--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,77 +3,235 @@ import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
 
 export class MainController {
-    $http;
+  $http;
 
-    awesomeThings = [];
-    newThing = '';
+  awesomeThings = [];
+  newThing = '';
 
-    /*@ngInject*/
-    constructor($http) {
-        this.$http = $http;
-    }
+  /*@ngInject*/
+  constructor($http) {
+    this.$http = $http;
+  }
 
-    $onInit() {
-        // JQUERY Functions
-        this.$http.get('/api/things')
-            .then(response => {
-                this.awesomeThings = response.data;
-            });
+  $onInit() {
+    // JQUERY Functions
 
-            // Load Tableau
-            var vizList = ["http://public.tableau.com/views/RegionalSampleWorkbook/Flights",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/Obesity",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/College",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/Stocks",
-            "http://public.tableau.com/views/RegionalSampleWorkbook/Storms"];
-        
-        var viz,
-            vizLen = vizList.length,
-            vizCount = 0;
-        
-        function createViz(vizPlusMinus) {
-            var vizDiv = document.getElementById("vizContainer"),
-                options = {
-                    hideTabs: true
-                };
-
-            vizCount = vizCount + vizPlusMinus;
-            
-            if (vizCount >= vizLen) { 
-            // Keep the vizCount in the bounds of the array index.
-                vizCount = 0;
-            } else if (vizCount < 0) {
-                vizCount = vizLen - 1;
-            }
-            
-            if (viz) { // If a viz object exists, delete it.
-                viz.dispose();
-            }
-
-            var vizURL = vizList[vizCount];
-            viz = new tableau.Viz(vizDiv, vizURL, options); 
+    Highcharts.chart('container', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Percent of Revenue Vehicles Above Useful Life'
+      },
+      colors: ['#db2929', '#00738C', '#307189', '#910000', '#1aadce',
+        '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'
+      ],
+      xAxis: {
+        categories: ['Light Rail/Trolley/Cable Car', 'Ferry', 'Bus', 'Heavy Rail', 'Cutaway/Van/Paratransit']
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Total Percentage (%)'
         }
-        createViz(3);
-    }
-
-    addThing() {
-        if (this.newThing) {
-            this.$http.post('/api/things', {
-                name: this.newThing
-            });
-            this.newThing = '';
+      },
+      tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+        shared: true
+      },
+      plotOptions: {
+        column: {
+          stacking: 'percent'
         }
-    }
+      },
+      series: [{
+        name: 'Above Useful Life',
+        data: [2, 2, 3, 2, 1]
+      }, {
+        name: 'Within Useful Life',
+        data: [5, 3, 4, 7, 2]
+      }]
+    });
 
-    deleteThing(thing) {
-        this.$http.delete(`/api/things/${thing._id}`);
+    // Pie Chart
+
+
+    // Make monochrome colors
+    var pieColors = (function () {
+      var colors = [],
+        base = '#00738C',
+        i;
+
+      for (i = 0; i < 10; i += 1) {
+        // Start out with a darkened base color (negative brighten), and end
+        // up with a much brighter color
+        colors.push(Highcharts.Color(base).brighten((i - 3) / 12).get());
+      }
+      return colors;
+    }());
+
+    // Build the chart
+    Highcharts.chart('pieChartContainer', {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Breakdown of Regional Revenue Vehicles by Mode'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          colors: pieColors,
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+            // distance: -50,
+            filter: {
+              property: 'total',
+              operator: '>',
+              value: 4
+            }
+          }
+          // showInLegend: true
+        }
+      },
+      series: [{
+        name: 'Brands',
+        data: [{
+            name: 'Bus',
+            y: 5963
+          },
+          {
+            name: 'Cutaway/Van/Paratransit',
+            y: 1374
+          },
+          {
+            name: 'Ferry',
+            y: 101
+          },
+          {
+            name: 'Heavy Rail',
+            y: 1110
+          },
+          {
+            name: 'Light Rail/Trolley/Cable Car',
+            y: 604
+          }
+
+        ]
+      }]
+    });
+
+    //Electric Vehicles
+
+Highcharts.chart('electricVehiclesContainer', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Fleet Breakdown by Fuel Type'
+    },
+    colors: ['#7CFC00', '#629632', '#00738C', '#910000', '#1aadce',
+    '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'
+],
+    xAxis: {
+        categories: ['AC Transit', 'CCCTA', 'Delta Breeze', 'Dixon Readi-Ride', 'ECCTA','FAST','Golden Gate Transit','LAVTA','Marin Transit','NVTA','Petaluma Transit','SamTrans','Santa Rosa Transit','SCT','SF Muni','Solano County Transit','UCT','Vacaville','VTA','WestCAT']
+    },
+    yAxis: {
+        min: 0,
+        max:100,
+        title: {
+            text: 'Fuel Type Percent of Total'
+        }
+    },
+    legend: {
+        reversed: true
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal',
+            pointWidth:25
+        }
+    },
+    series: [ {
+        name: 'Zero Emissions',
+        data: [5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5]
+    },{
+        name: 'Hybrid',
+        data: [5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5,5, 5, 5, 5, 5]
+    },{
+        name: 'Diesel/Combustion Fuel',
+        data: [90, 90, 90, 90, 90,90, 90, 90, 90, 90,90, 90, 90, 90, 90,90, 90, 90, 90, 90]
+    }]
+});
+
+    this.$http.get('/api/things')
+      .then(response => {
+        this.awesomeThings = response.data;
+      });
+
+    // Load Tableau
+    var vizList = ["http://public.tableau.com/views/RegionalSampleWorkbook/Flights",
+      "http://public.tableau.com/views/RegionalSampleWorkbook/Obesity",
+      "http://public.tableau.com/views/RegionalSampleWorkbook/College",
+      "http://public.tableau.com/views/RegionalSampleWorkbook/Stocks",
+      "http://public.tableau.com/views/RegionalSampleWorkbook/Storms"
+    ];
+
+    var viz,
+      vizLen = vizList.length,
+      vizCount = 0;
+
+    function createViz(vizPlusMinus) {
+      var vizDiv = document.getElementById("vizContainer"),
+        options = {
+          hideTabs: true
+        };
+
+      vizCount = vizCount + vizPlusMinus;
+
+      if (vizCount >= vizLen) {
+        // Keep the vizCount in the bounds of the array index.
+        vizCount = 0;
+      } else if (vizCount < 0) {
+        vizCount = vizLen - 1;
+      }
+
+      if (viz) { // If a viz object exists, delete it.
+        viz.dispose();
+      }
+
+      var vizURL = vizList[vizCount];
+      viz = new tableau.Viz(vizDiv, vizURL, options);
     }
+    // createViz(3);
+  }
+
+  addThing() {
+    if (this.newThing) {
+      this.$http.post('/api/things', {
+        name: this.newThing
+      });
+      this.newThing = '';
+    }
+  }
+
+  deleteThing(thing) {
+    this.$http.delete(`/api/things/${thing._id}`);
+  }
 }
 
 export default angular.module('rtciApp.main', [uiRouter])
-    .config(routing)
-    .component('main', {
-        template: require('./main.html'),
-        controller: MainController
-    })
-    .name;
+  .config(routing)
+  .component('main', {
+    template: require('./main.html'),
+    controller: MainController
+  })
+  .name;
